be4d8698079dcaf22ccec95afeb6e1fdc8f59afc
JUNGHOJONG <hojong1351@naver.com>
2021-09-12 20:58:27 +0900

    상품 도메인 개발 및 테스트

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -1,2 +1,40 @@
-package hello.itemservice.domain.item;public class ItemRepository {
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private final static Map<Long, Item> itemStore = new HashMap<>();
+    private static Long sequence = 0L;
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        itemStore.put(sequence, item);
+        return item;
+    }
+
+    public void update(Long itemId, Item updateItem) {
+        Item savedItem = itemStore.get(itemId);
+        savedItem.setItemName(updateItem.getItemName());
+        savedItem.setPrice(updateItem.getPrice());
+        savedItem.setQuantity(updateItem.getQuantity());
+    }
+
+    public Item findById(Long itemId) {
+        return itemStore.get(itemId);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(itemStore.values());
+    }
+
+    public void clearItemStore() {
+        itemStore.clear();
+    }
 }
--- src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -1,4 +1,66 @@
-import static org.junit.jupiter.api.Assertions.*;
+package hello.itemservice.domain.item;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 class ItemRepositoryTest {
-  
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach() {
+        itemRepository.clearItemStore();
+    }
+
+    @Test
+    void saveTest() {
+        // GIVEN
+        Item item = new Item("itemA", 10000, 10);
+
+        itemRepository.save(item);
+
+        // WHEN
+        Item savedItem = itemRepository.findById(item.getId());
+
+        // THEN
+        assertThat(savedItem).isEqualTo(item);
+    }
+
+    @Test
+    void findAllTest() {
+        // GIVEN
+        Item item1 = new Item("itemA", 10000, 10);
+        Item item2 = new Item("itemB", 20000, 30);
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+
+        // WHEN
+        List<Item> savedItems = itemRepository.findAll();
+
+        // THEN
+        assertThat(savedItems).contains(item1, item2);
+    }
+
+    @Test
+    void updateTest() {
+        // GIVEN
+        Item item = new Item("itemA", 10000, 10);
+        Item savedItem = itemRepository.save(item);
+
+        // WHEN
+        Item item2 = new Item("itemB", 20000, 30);
+        itemRepository.update(savedItem.getId(), item2);
+        Item updateItem = itemRepository.findById(savedItem.getId());
+
+        // THEN
+        assertThat(updateItem.getItemName()).isEqualTo(item2.getItemName());
+        assertThat(updateItem.getPrice()).isEqualTo(item2.getPrice());
+        assertThat(updateItem.getQuantity()).isEqualTo(item2.getQuantity());
+    }
+
 }
